f6218d1c9aac77f0ea6df2b922fa7d696305f242
RockyRori <luosuhairocky@gmail.com>
2025-08-08 10:04:28 +0800

    [feature]注释部分功能

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,23 @@
+import os
 
-def identify_docx_labels(docx_folder, zip_from = ''):
+from docx import Document
+
+
+def identify_docx_labels(docx_folder, zip_from=''):
     """
     遍历指定目录下的所有 .docx 文件，判断每个文件是商务标、技术标还是价格标。
     
     :param directory: str, 待检查的目录路径
     :return: list of dict, 每个字典表示一个文件及其标签
     """
+
     def check_file_content(doc_path, keywords):
         """检查文件内容中是否包含指定的关键字"""
         try:
             doc = Document(doc_path)
             for paragraph in doc.paragraphs:
                 if paragraph.text == keywords:
-                        return True
+                    return True
                 for run in paragraph.runs:
                     if run.text == keywords:
                         return True
@@ -34,7 +39,7 @@ def identify_docx_labels(docx_folder, zip_from = ''):
         for filename in filenames:
             file_path = os.path.join(rootpath, filename)
             matched_labels = []
-            
+
             # if filename.endswith('.doc'):
             #     cres = doc_to_docx(file_path)
             #     if cres:
@@ -52,24 +57,24 @@ def identify_docx_labels(docx_folder, zip_from = ''):
                     if check_file_content(file_path, info["content_keyword"]):
                         matched_labels.append(label)
 
-            elif filename.endswith('.doc'):
-                cres = doc_to_docx(file_path)
-                if cres:
-                    for label, info in labels.items():
-                        if check_file_content(cres, info["content_keyword"]):
-                            matched_labels.append(label)
-            
+            # elif filename.endswith('.doc'):
+            #     cres = doc_to_docx(file_path)
+            #     if cres:
+            #         for label, info in labels.items():
+            #             if check_file_content(cres, info["content_keyword"]):
+            #                 matched_labels.append(label)
+
             elif (filename.endswith('.xlsx') or filename.endswith('.xls')) and ('商务' in zip_from or '价' in filename):
                 matched_labels.append('价格')
-                
+
             elif filename.endswith('.dwg'):
                 matched_labels.append('技术')
 
             elif '技术' in zip_from:
                 matched_labels.append('技术')
-                
+
             # 去重
             matched_labels = list(set(matched_labels))
             result[filename] = matched_labels
-        
+
     return result
